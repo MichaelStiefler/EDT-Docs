24b306d7129ab9408b0ce67b8e60fa0a94d624dc
Mike (Storebror) <stiefler@casio.de>
2020-09-17 16:00:13 +0200

    EDT 2.0 WIP 2020-09-17_001 Commit

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"jp.co.casio.edt"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.casioeurope.mis.edt"},{"l":"com.casioeurope.mis.edt.types"}];updateSearchResults();
