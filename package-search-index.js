c5d521c7a8982b8a1c006c4353bb48c7e35b62a2
Michael Stiefler <stiefler@casio.de>
2020-09-17 16:17:38 +0200

    EDT 2.0 WIP 2020-09-17_002 Commit

--- package-search-index.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.casioeurope.mis.edt"}]
-=======
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.casioeurope.mis.edt"},{"l":"com.casioeurope.mis.edt.types"}];updateSearchResults();
->>>>>>> master
